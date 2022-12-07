4585bb2107d471a49cf1c15b8b5e207b78c524d6
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2022-12-07 15:28:24 +0600

    Merge pdf files

--- pdf_tool/main.py
+++ pdf_tool/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+from typing import List
 
 import typer
+from PyPDF2 import PdfMerger
 from rich import print
+from rich.progress import track
 
 from .commands.docs import app as docs_app
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 app.add_typer(docs_app, name="docs", help="Generate documentation")
 
 
@@ -23,3 +26,18 @@ def ls():
 
     typer.echo("Listing files in current directory:\n")
     print(dirs)
+
+
+@app.command()
+def merge(pdfs: List[Path], output_file: str):
+    """
+    Merge pdf files in a specified order
+    """
+
+    typer.echo("Merging PDF files:\n")
+    merger = PdfMerger()
+
+    for pdf in track(pdfs, description="Processing..."):
+        merger.append(pdf)
+    merger.write(output_file)
+    merger.close()
